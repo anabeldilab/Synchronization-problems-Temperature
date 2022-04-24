f07e19a05b138aa6b03f8798b9d9bf440681102f
alu0101206011 <alu0101206011@ull.edu.es>
2022-04-24 19:03:18 +0100

    Works producer-consumer with two consumers

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ void Producer() {
         Comparator++;
 
         mutex.lock();
-        if (Comparator >= SampleSize)
+        if (Comparator >= SampleSize*2)
             bufferNotEmpty.wakeAll();
         mutex.unlock();
     }
@@ -55,19 +55,18 @@ void Producer() {
 
 
 
-void Consumer() {
-    for (unsigned int i = 0; i < TotalData; i++) {
-        if (i % SampleSize == 0) {
+void Consumer(unsigned int start_pos) {
+    unsigned int counter = start_pos;
+    while (counter < TotalData) {
+        if (counter % SampleSize == 0) {
             mutex.lock();
             if (Comparator < SampleSize)
                bufferNotEmpty.wait(&mutex);
             mutex.unlock();
 
-            getMean(&Buffer, i % TotalBufferSize);
-            median(&Buffer, i % TotalBufferSize);
-            std::cout << i << " of " << TotalData << " ";
-            std::cout << "average temperature " << getMean(&Buffer, i % TotalBufferSize) << " ";
-            std::cout << " with median " << median(&Buffer, i % TotalBufferSize) << " ";
+            std::cout << counter << " of " << TotalData << " ";
+            std::cout << "average temperature " << getMean(&Buffer, counter % TotalBufferSize) << " ";
+            std::cout << " with median " << median(&Buffer, counter % TotalBufferSize) << " ";
             std::cout << "Current index: " << Comparator << "\n";
             std::cout.flush();
 
@@ -76,6 +75,7 @@ void Consumer() {
             bufferNotFull.wakeAll();
             mutex.unlock();
         }
+        counter += SampleSize*2;
     }
 }
 
@@ -88,8 +88,6 @@ int main(int argc, char *argv[]) {
         Buffer.push_back(random() % 50 + 50);
         if(i % (24*3600) == 0) {
             Statistics s(Buffer);
-            s.getMean();
-            s.median();
             std::cout << i << " of " << TotalData << " ";
             std::cout << "average temperature " << s.getMean() << " ";
             std::cout << " with median " << s.median() << "\n";
@@ -105,16 +103,19 @@ int main(int argc, char *argv[]) {
 
     Buffer.resize(TotalBufferSize);
     auto start1 = std::chrono::high_resolution_clock::now();
-    std::thread p(Producer),c1(Consumer); //, c2(Consumer);
+    std::thread p(Producer),c1(Consumer, 0), c2(Consumer, SampleSize);
     p.join();
     c1.join();
-    //c2.join();
+    c2.join();
     auto stop1 = std::chrono::high_resolution_clock::now();
     auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop1 - start1);
     std::cout << duration2.count() << "\n";
     std::cout << "Done in producer/consumer mode\n";
 
     std::cout << "|Serial mode\t|  producer consumer mode\t|\n|" << duration1.count() << "\t|  " << duration2.count() << "\t\t\t|\n";
-    std::cout << "The producer consumer mode is " << duration1.count() / duration2.count() << " times faster\n";
+    float serial_time = duration1.count();
+    float producer_consumer_time = duration2.count();
+    std::cout << "The producer consumer mode is " << serial_time/producer_consumer_time << " times faster\n";
+
     return 0;
 }
