0483d79eeca1ff7f2622079081c9a41d0dfd145c
alu0101206011 <alu0101206011@ull.edu.es>
2022-04-24 20:24:22 +0100

    Works readers-writers mode and matching serial mode

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <QWaitCondition>
 #include <QMutex>
+#include <QReadWriteLock>
 #include <condition_variable>
 #include <thread>
 #include <unistd.h>
@@ -67,7 +68,7 @@ void Consumer(unsigned int start_pos) {
             std::cout << counter << " of " << TotalData << " ";
             std::cout << "average temperature " << getMean(&Buffer, counter % TotalBufferSize) << " ";
             std::cout << " with median " << median(&Buffer, counter % TotalBufferSize) << " ";
-            std::cout << "Current index: " << Comparator << "\n";
+            std::cout << "Current producer index: " << Comparator << "\n";
             std::cout.flush();
 
             mutex.lock();
@@ -79,43 +80,92 @@ void Consumer(unsigned int start_pos) {
     }
 }
 
+QReadWriteLock lock;
+
+void Writer() {
+    for (unsigned int i = 0; i < TotalData; i++) {
+        lock.lockForWrite();
+        if (Comparator == TotalBufferSize) {
+            bufferNotFull.wait(&lock);
+        }
+        lock.unlock();
+
+        Buffer[i % TotalBufferSize] = random() % 50 + 50;
+        Comparator++;
+
+        lock.lockForWrite();
+        if (Comparator >= SampleSize*2)
+            bufferNotEmpty.wakeAll();
+        lock.unlock();
+    }
+}
+
+void Reader(unsigned int start_pos) {
+    unsigned int counter = start_pos;
+    while (counter < TotalData) {
+        if (counter % SampleSize == 0) {
+            lock.lockForRead();
+            if (Comparator < SampleSize)
+               bufferNotEmpty.wait(&lock);
+            lock.unlock();
+
+            std::cout << counter << " of " << TotalData << " ";
+            std::cout << "average temperature " << getMean(&Buffer, counter % TotalBufferSize) << " ";
+            std::cout << " with median " << median(&Buffer, counter % TotalBufferSize) << " ";
+            std::cout << "Current writer index: " << Comparator << "\n";
+            std::cout.flush();
+
+            lock.lockForRead();
+            Comparator = Comparator - SampleSize;
+            bufferNotFull.wakeAll();
+            lock.unlock();
+        }
+        counter += SampleSize*2;
+    }
+}
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+    Buffer.resize(TotalBufferSize);
 
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start1 = std::chrono::high_resolution_clock::now();
     for(unsigned int i = 0; i < TotalData; i++) {
-        Buffer.push_back(random() % 50 + 50);
+        Buffer[i % TotalBufferSize] = random() % 50 + 50;
         if(i % (24*3600) == 0) {
-            Statistics s(Buffer);
             std::cout << i << " of " << TotalData << " ";
-            std::cout << "average temperature " << s.getMean() << " ";
-            std::cout << " with median " << s.median() << "\n";
-            Buffer.erase(Buffer.constBegin(),Buffer.constEnd());
+            std::cout << "average temperature " << getMean(&Buffer, i % TotalBufferSize) << " ";
+            std::cout << " with median " << median(&Buffer, i % TotalBufferSize) << "\n";
         }
     }
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    auto stop1 = std::chrono::high_resolution_clock::now();
+    auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(stop1 - start1);
     std::cout << duration1.count() << "\n";
     std::cout << "Done in serial mode\n";
 
-
-
-    Buffer.resize(TotalBufferSize);
-    auto start1 = std::chrono::high_resolution_clock::now();
+    auto start2 = std::chrono::high_resolution_clock::now();
     std::thread p(Producer),c1(Consumer, 0), c2(Consumer, SampleSize);
     p.join();
     c1.join();
     c2.join();
-    auto stop1 = std::chrono::high_resolution_clock::now();
-    auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop1 - start1);
+    auto stop2 = std::chrono::high_resolution_clock::now();
+    auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop2 - start2);
     std::cout << duration2.count() << "\n";
     std::cout << "Done in producer/consumer mode\n";
 
-    std::cout << "|Serial mode\t|  producer consumer mode\t|\n|" << duration1.count() << "\t|  " << duration2.count() << "\t\t\t|\n";
-    float serial_time = duration1.count();
+    auto start3 = std::chrono::high_resolution_clock::now();
+    std::thread w(Writer), r1(Reader, 0), r2(Reader, SampleSize);
+    w.join();
+    r1.join();
+    r2.join();
+    auto stop3 = std::chrono::high_resolution_clock::now();
+    auto duration3 = std::chrono::duration_cast<std::chrono::microseconds>(stop3 - start3);
+    std::cout << duration3.count() << "\n";
+    std::cout << "Done in readers writer mode\n";
+
+    std::cout << "|  Serial mode\t|  producer consumer mode\t|  readers writer mode\t\n|  " << duration1.count() << "\t|  " << duration2.count() << "\t\t\t|  " << duration3.count() << "\t\n";
     float producer_consumer_time = duration2.count();
-    std::cout << "The producer consumer mode is " << serial_time/producer_consumer_time << " times faster\n";
+    float readers_writer_time = duration3.count();
+    std::cout << "The readers writer mode is " << producer_consumer_time/readers_writer_time << " times faster\n";
 
     return 0;
 }
